a9616c5254f462b783b83030a6fd86135fa317b8
Manguardia <49807597+Manguardia@users.noreply.github.com>
2019-06-15 19:55:57 -0400

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Variables a rellenar */
-	
-$favicon = ''; /* URI del favicon de la página  ej: https://manguardia.github.io/radio-manguardia/favicon.ico */
-$logo = '';  /* URI del logo de la página (tamaño máximo recomendado para el logo 60x60px) ej: https://github.com/Manguardia/radio-manguardia/blob/master/Tapestack_colorway001_1500x1500.jpg/
-$destacados =; /* Número de la categoría a destacar (los números aparecen en el menú de administración en Gestionar>>Categorías*/
-$titulo=''; /* Nombre con el que referirse a los 5 últimos posts de la categoría destacada, por ejemplo "Ultimos destacados" */         
-
-?>>
